7e879083a78de7116849be7e9f78a9f56415b004
rohangarg1998 <rohan.grg1998@gmail.com>
2018-03-01 22:42:55 +0530

    add changecolor.js

---
+++ colorchange.js
@@ -0,0 +1,19 @@
+var all = document.getElementsByTagName("*");
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function changeColor() { 
+let a = x ;
+  window.setInterval(function(){
+     a.style.backgroundColor = getRandomColor();
+  }, 150);
+}
+for (var i=0, max=all.length; i < max; i++) {
+	var x = all[i];
+    changeColor();
+}
